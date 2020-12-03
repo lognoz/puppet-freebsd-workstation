bbf90956cf8c657d6d9624f80b08024fe2cbc01b
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-03 03:00:30 +0000

    Manifests: Add sudo class to manage root privileges

---
+++ manifests/sudo.pp
@@ -0,0 +1,21 @@
+# FreeBSD Sudo Support
+#
+# This class initialize sudo package. This program is designed to
+# allow a sysadmin to give limited root privileges to users and log
+# root activity.
+#
+class workstation::sudo {
+  # Make sure this subclass is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  } else {
+    include workstation
+  }
+
+  class { 'sudo': }
+
+  sudo::conf { "${workstation::username}-all":
+    priority => 10,
+    content  => "${workstation::username} ALL=(ALL) ALL",
+  }
+}
