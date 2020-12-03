374b44cc95a96825e35526e47c2fc151f97bba70
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-03 02:54:16 +0000

    Manifests: Add subclass to manage power control

---
+++ manifests/powerd.pp
@@ -0,0 +1,24 @@
+# FreeBSD Powerd Support
+#
+# This class initialize powerd package. This program utility monitors
+# the system state and sets various power control options accordingly.
+#
+class workstation::powerd {
+  # Make sure this subclasses is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  }
+
+  package { 'powerdxx': }
+
+  file_line { 'Disable powerd in /etc/rc.conf':
+    path   => '/etc/rc.conf',
+    line   => 'powerd_enable="NO"',
+    match  => 'powerd_enable="YES"'
+  }
+
+  file_line { 'Enable powerdxx in /etc/rc.conf':
+    path => '/etc/rc.conf',
+    line => 'powerdxx_enable="YES"'
+  }
+}
