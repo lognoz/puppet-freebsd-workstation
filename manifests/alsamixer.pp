cefb43643e2043ca4c32d9e5c34f929a0708cfe1
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-03 02:57:10 +0000

    Manifests: Add subclass to manage sound architecture

---
+++ manifests/alsamixer.pp
@@ -0,0 +1,14 @@
+# FreeBSD Alsamixer Support
+#
+# This class initialize alsamixer package. This program is a graphical
+# mixer program for the Advanced Linux Sound Architecture that is used
+# to configure sound settings and adjust the volume.
+#
+class workstation::alsamixer {
+  # Make sure this subclass is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  }
+
+  package { 'alsa-utils': }
+}
