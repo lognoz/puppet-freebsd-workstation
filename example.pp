ed3d3662b2a9e9058ead8f8ebe74609570d8b7bd
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-04 20:46:04 +0000

    Example: Add new examples usage in puppet node

--- example.pp
+++ example.pp
@@ -4,4 +4,23 @@ node default {
     password => 'default',
     timezone => 'Canada/Eastern'
   }
+
+  class { 'workstation::graphic':
+    hardware => 'nvidia'
+  }
+
+  class { 'workstation::git':
+    username  => 'Marc-Antoine Loignon',
+    email => 'developer@lognoz.org'
+  }
+
+  include workstation::sudo
+  include workstation::system
+  include workstation::package
+  include workstation::xorg
+
+  class { 'workstation::languages':
+    keyboard => 'us,ca',
+    aspell => ['en','fr']
+  }
 }
