50b6a137c3f18806095f5dfa07b7d8d86d12df44
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-04 20:25:53 +0000

    Manifests: Add graphic support to make freebsd usable on desktop

---
+++ manifests/graphic.pp
@@ -0,0 +1,47 @@
+# FreeBSD Graphics Support
+#
+# This class add graphics support to make FreeBSD usable as a desktop.
+# It only supports Intel HD and NVIDIA graphics cards.
+#
+# $hardware   String used to install the right graphic card.
+#             It expected to recives 'intel' or 'nvidia'.
+#
+class workstation::graphic (
+  String $hardware = undef
+) {
+  # Make sure this subclass is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  }
+
+  if $hardware == 'intel' {
+    package { 'drm-kmod': ensure => installed }
+
+    file_line { 'Add kld to /etc/rc.conf':
+      ensure => present,
+      path   => '/etc/rc.conf',
+      line   => 'kld_list="/boot/modules/i915kms.ko"',
+      match  => '^kld_list'
+    }
+  }
+  elsif $hardware == 'nvidia' {
+    package { 'nvidia-driver': ensure => installed }
+
+    file_line { 'Add nvidia to /boot/loader.conf':
+      ensure => present,
+      path   => '/boot/loader.conf',
+      line   => 'nvidia_load="YES"',
+      match  => '^nvidia_load'
+    }
+
+    file_line { 'Add kld to /etc/rc.conf':
+      ensure => present,
+      path   => '/etc/rc.conf',
+      line   => 'kld_list="nvidia-modeset"',
+      match  => '^kld_list'
+    }
+  }
+  else {
+    fail("Parameter hardware '${hardware}' is not supported.")
+  }
+}
