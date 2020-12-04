4d5284e86939853477faf6b0cf407c4b1eabe1c9
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-04 22:59:59 +0000

    Manifests: Add initial font component

---
+++ manifests/fonts.pp
@@ -0,0 +1,22 @@
+# Class: workstation::fonts
+#
+# This class install system fonts.
+#
+# Requires:
+#   Class workstation
+#
+# Sample Usage:
+#   include workstation::fonts
+#
+class workstation::fonts {
+  # Make sure this subclass is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  }
+
+  package { 'sourcecodepro-ttf': }
+
+  workstation::x11::conf {
+    'FontPath "/usr/local/share/fonts/SourceCodePro/"':
+  }
+}
