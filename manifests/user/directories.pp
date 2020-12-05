e944a0319cc5cabe353a4f54a078f62a59517c61
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-05 23:26:47 +0000

    Manifests: Add class that manages user directories preferences

---
+++ manifests/user/directories.pp
@@ -0,0 +1,42 @@
+# Class: workstation::user:directories
+#
+# Requires:
+#   Class workstation
+#
+# Sample Usage:
+#   include workstation::user:directories
+#
+class workstation::user::directories (
+  String $desktop  = '/',
+  String $document = '/document',
+  String $download = '/download',
+  String $music    = '/music',
+  String $picture  = '/picture',
+  String $public   = '/public',
+  String $template = '/template',
+  String $video    = '/video',
+) {
+  # Make sure this subclass is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  }
+
+  package {
+    'xdg-user-dirs':
+  }
+
+  File {
+    owner => $workstation::username,
+    group => $workstation::username,
+    mode  => '0755'
+  }
+
+  file { "/home/${workstation::username}/.config":
+    ensure => directory
+  }
+
+  file { "/home/${workstation::username}/.config/user-dirs.dirs":
+    ensure  => present,
+    content => template('workstation/user-dirs.erb')
+  }
+}
