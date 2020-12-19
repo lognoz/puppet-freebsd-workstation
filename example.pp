fa4366ec921d709d947c3329d05c977d51e47561
Marc-Antoine Loignon <developer@lognoz.org>
2020-12-19 03:52:54 +0000

    Example: Add statements to install web components and system requirements

--- example.pp
+++ example.pp
@@ -5,22 +5,39 @@ node default {
     timezone => 'Canada/Eastern'
   }
 
-  class { 'workstation::graphic':
-    hardware => 'nvidia'
-  }
-
-  class { 'workstation::git':
-    username  => 'Marc-Antoine Loignon',
-    email => 'developer@lognoz.org'
-  }
-
   include workstation::sudo
   include workstation::system
   include workstation::package
-  include workstation::xorg
+  include workstation::x11::xorg
+
+  workstation::x11::conf { [
+    'setxkbmap -option caps:escape &',
+    'xrandr --output DVI-I-1 --right-of HDMI-0 &'
+  ]: }
 
   class { 'workstation::languages':
     keyboard => 'us,ca',
     aspell => ['en','fr']
   }
+
+  class { 'workstation::graphic':
+    hardware => 'nvidia'
+  }
+
+  class { 'workstation::user::git':
+    username  => 'Marc-Antoine Loignon',
+    email => 'developer@lognoz.org'
+  }
+
+  include workstation::x11::dwm
+  include workstation::user::directories
+
+  class { 'workstation::user::vim':
+    source => 'https://github.com/lognoz/vimrc'
+  }
+
+  include workstation::web::php
+  include workstation::web::mysql
+  include workstation::web::apache
+  include workstation::web::npm
 }
