079e11359dc396a8735a64c57176e532bc21714b
Marc-Antoine Loignon <developer@lognoz.org>
2021-01-09 18:48:12 +0000

    Manifests: Add intial youtube-dl component

---
+++ manifests/multimedia/youtube_dl.pp
@@ -0,0 +1,24 @@
+# Class: workstation::multimedia::youtube_dl
+#
+# Requires:
+#   Class workstation
+#
+# Sample Usage:
+#   include workstation::multimedia::youtube_dl
+#
+class workstation::multimedia::youtube_dl (
+  String $directory = 'video'
+) {
+  # Make sure this subclass is executed after workstation is loaded.
+  if ! defined(Class['workstation']) {
+    fail('You must include the base workstation class before using any subclasses.')
+  } else {
+    include workstation
+  }
+
+  package { 'youtube_dl': }
+
+  workstation::bash::rc {
+    "alias youtube-dl='youtube-dl -o \"~/${directory}/%(title)s-%(id)s.%(ext)s\"'":
+  }
+}
