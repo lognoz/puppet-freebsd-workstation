20d974bac501d7795d257a933c502ad6c96b092b
Marc-Antoine Loignon <developer@lognoz.org>
2021-08-10 08:06:23 -0400

    Manifests: Add vulnerability section to sudo manifest

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -7,6 +7,15 @@
 # Requires:
 #   Class workstation
 #
+# Vulnerability:
+#
+#   I now use doas instead of sudo because a severe vulnerability was
+#   found in Unix and Linux operating systems that allows an
+#   unprivileged user to exploit this vulnerability using sudo, causing
+#   a heap overflow to elevate privileges to root without
+#   authentication, or even get listed in the sudoers
+#   file. (CVE-2021-3156)
+#
 # Sample Usage:
 #   include workstation::sudo
 #
