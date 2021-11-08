5990ec870f6d1de6ad9c0a9b23976b5140a504fe
Marc-Antoine Loignon <developer@lognoz.org>
2021-11-08 10:28:44 -0500

    Python: Add variable to manages what version to use

--- manifests/programming/python.pp
+++ manifests/programming/python.pp
@@ -2,19 +2,23 @@
 #
 # This class initialize Python programming language.
 #
+# Variables:
+#   `version` — Type: *string* — Default: *38*
+#   The python version that need to be installed.
+#
 # Requires:
 #   Class workstation
 #
 # Sample Usage:
 #   include workstation::programming::python
 #
-class workstation::programming::python {
+class workstation::programming::python (
+  String $version = '38'
+) {
   # Make sure this subclass is executed after workstation is loaded.
   if ! defined(Class['workstation']) {
     fail('You must include the base workstation class before using any subclasses.')
   }
-
-  $version = '38'
 
   package { [
     "python${version}",
